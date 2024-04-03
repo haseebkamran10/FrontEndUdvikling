8819e869ab9dcaca143e9aa15e6bbc21b9aa0edc
haseebkamran10 <s224270@dtu.dk>
2024-04-04 00:51:03 +0200

    Trying to fix the transition effect
    Trying to fix the transition effect

--- my-app/src/pages/LoginPage/LoginPage.tsx
+++ my-app/src/pages/LoginPage/LoginPage.tsx
@@ -1,17 +1,28 @@
+import React from 'react';
 import './LoginPage.css';
 import CloseIcon from '@mui/icons-material/Close';
 import SportsCricketIcon from '@mui/icons-material/SportsCricket';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
-const LoginPage = () => {
+// Adding the isVisible prop to the component's props type
+interface LoginPageProps {
+  isVisible: boolean; // This prop dictates if the login page is visible
+  onClose: () => void; // Function to call when closing the login page
+}
+
+const LoginPage: React.FC<LoginPageProps> = ({ isVisible, onClose }) => {
+  // Apply 'show' class based on the isVisible prop
+  const containerClasses = `login-page-cont${isVisible ? ' show' : ''}`;
+
   return (
-    <div className='login-page-cont' style={{display: 'flex', flexDirection: 'column'}}>
+    <div className={containerClasses} style={{ display: 'flex', flexDirection: 'column' }}>
       <div className='right-cont'>
-        <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
           <h3>Log ind</h3>
-          <CloseIcon />
+          {}
+          <CloseIcon onClick={onClose} />
         </div>
-        <p style={{textAlign: "left"}}>Log ind på din konto:</p>
+        <p style={{ textAlign: "left" }}>Log ind på din konto:</p>
         <form action="#">
           <input type="email" name="" id="" placeholder='E-mail' />
           <input type="password" name="" id="" placeholder='Adgangskode' />
@@ -20,14 +31,14 @@ const LoginPage = () => {
           <button className='btn1'>Glemt adgangskode</button>
           <button className='btn2'>Log ind</button>
         </div>
-        <h3 style={{textAlign: "left", fontSize: "1.0rem"}}>Ikke medlem endnu</h3>
-        <div style={{display: "flex", alignItems: "center"}}>
+        <h3 style={{ textAlign: "left", fontSize: "1.0rem" }}>Ikke medlem endnu</h3>
+        <div style={{ display: "flex", alignItems: "center" }}>
           <SportsCricketIcon />
-          <p style={{marginLeft: "15px", color: "gray"}}>Bliv medlem</p>
+          <p style={{ marginLeft: "15px", color: "gray" }}>Bliv medlem</p>
         </div>
-        <div style={{display: "flex", alignItems: "center"}}>
+        <div style={{ display: "flex", alignItems: "center" }}>
           <ErrorOutlineIcon />
-          <p style={{marginLeft: "15px", color: "gray"}}>Læs om alle fordele</p>
+          <p style={{ marginLeft: "15px", color: "gray" }}>Læs om alle fordele</p>
         </div>
       </div>
     </div>
