5c4701333e2f80ec41228f0e6a4593087f4614fb
haseebkamran10 <s224270@dtu.dk>
2024-04-04 00:22:24 +0200

    Moving login component to the right
    Moving login component to the right

--- my-app/src/components/header/header.tsx
+++ my-app/src/components/header/header.tsx
@@ -1,64 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './header.css';
 import MenuIcon from '../../menu.svg';
 import Logo from '../../logo.svg';
 import SearchIcon from '../../search-icon.svg';
 import UserIcon from '../../user-icon.svg';
 import ShoppingCartIcon from '../../shopping-cart-icon.svg';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
-  const [showLogin, setShowLogin] = useState(false);
-
-  // This will handle clicking on the backdrop to hide the login form
-  const closeLogin = () => {
-    setShowLogin(false);
-  };
-
-  const toggleLogin = () => {
-    setShowLogin(!showLogin);
-  };
-
   return (
-    <>
-      <div className="header-container">
-        <div className="left-section">
-          <img src={MenuIcon} alt="Menu" className="menu-icon" />
-          <span className="page-title">Cricket Bats</span>
-          <span className="page-title">Cricket Sko</span>
-          <span className="page-title">Udstyr</span>
-        </div>
-        <div className="center-section">
-          <img src={Logo} alt="Logo" className="logo" />
-        </div>
-        <div className="right-section">
-          <div className="search-box">
-            <img src={SearchIcon} alt="Search" className="search-icon" />
-            <input type="text" placeholder="Hvad leder du efter?" className="search-input" />
-          </div>
-          <img src={ShoppingCartIcon} alt="Shopping Cart" className="shopping-cart-icon" />
-          <img
-            src={UserIcon}
-            alt="User"
-            className="user-icon"
-            onClick={toggleLogin} // Trigger the login popup
-          />
-        </div>
+    <div className="header-container">
+      <div className="left-section">
+        <img src={MenuIcon} alt="Menu" className="menu-icon" />
+        <span className="page-title">Cricket Bats</span>
+        <span className="page-title">Cricket Sko</span>
+        <span className="page-title">Udstyr</span>
       </div>
-      {showLogin && (
-        <div className="login-container">
-          <div className="backdrop" onClick={closeLogin}></div>
-          <div className="login-popup">
-            <div className="login-popup-close" onClick={closeLogin}>×</div>
-            <div className="login-popup-content">
-              <h2>Log ind</h2>
-              <input type="email" placeholder="E-mail" />
-              <input type="password" placeholder="Adgangskode" />
-              <button>Log ind</button>
-            </div>
-          </div>
+      <div className="center-section">
+        <img src={Logo} alt="Logo" className="logo" />
+      </div>
+      <div className="right-section">
+        <div className="search-box">
+          <img src={SearchIcon} alt="Search" className="search-icon" />
+          <input type="text" placeholder="Hvad leder du efter" className="search-input" />
         </div>
-      )}
-    </>
+        <img src={ShoppingCartIcon} alt="Shopping Cart" className="shopping-cart-icon" />
+        
+        <Link to = '/login'>
+         <img src={UserIcon} alt="User" className="user-icon" />
+        </Link>
+      </div>
+    </div>
   );
 };
 
--- my-app/src/pages/LoginPage/LoginPage.tsx
+++ my-app/src/pages/LoginPage/LoginPage.tsx
@@ -1,46 +1,37 @@
-import './LoginPage.css'
-import login from '../../login.png'
+import './LoginPage.css';
 import CloseIcon from '@mui/icons-material/Close';
 import SportsCricketIcon from '@mui/icons-material/SportsCricket';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 
-const LoginPage =()=>{
-
-return(
-  <div className='login-page-cont' style={{display:'flex', flexDirection:'column'}}>
-     <div style={{display:'flex'}}>
-        <div className='loft-cont'>
-            <img src={login} alt="" />
+const LoginPage = () => {
+  return (
+    <div className='login-page-cont' style={{display: 'flex', flexDirection: 'column'}}>
+      <div className='right-cont'>
+        <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+          <h3>Log ind</h3>
+          <CloseIcon />
         </div>
-        <div className=' right-cont'>
-            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }} >
-                        <h3>Log ind</h3>
-                         <CloseIcon />
-                    </div>
-                    <p style={{ textAlign: "left" }} >Log ind på din konto:</p>
-                    <form action="#">
-                        <input type="email" name="" id="" placeholder='E-mail' />
-                        <input type="password" name="" id="" placeholder='Adgangskode' />
-                    </form>
-                    <div className='btns-cont' >
-                        <button className='btn1' >Glemt adgangskode</button>
-                        <button className='btn2' >Log ind</button>
-                    </div>
-                    <h3 style={{ textAlign: "left", fontSize: "1.0rem" }} >Ikke medlem endnu</h3>
-                    <div className='' style={{ display: "flex", alignItems: "center" }} >
-                        <SportsCricketIcon />
-                        <p className='' style={{ marginLeft: "15px", color: "gray" }} >Bliv medlem</p>
-                    </div>
-                    <div style={{ display: "flex", alignItems: "center" }} >
-                        <ErrorOutlineIcon />
-                        <p style={{ marginLeft: "15px", color: "gray" }} >Læs om alle fordele</p>
-                    </div>                  
+        <p style={{textAlign: "left"}}>Log ind på din konto:</p>
+        <form action="#">
+          <input type="email" name="" id="" placeholder='E-mail' />
+          <input type="password" name="" id="" placeholder='Adgangskode' />
+        </form>
+        <div className='btns-cont'>
+          <button className='btn1'>Glemt adgangskode</button>
+          <button className='btn2'>Log ind</button>
         </div>
-     </div>
-     <h3>ANBEAFALING TI</h3>
-  </div>
+        <h3 style={{textAlign: "left", fontSize: "1.0rem"}}>Ikke medlem endnu</h3>
+        <div style={{display: "flex", alignItems: "center"}}>
+          <SportsCricketIcon />
+          <p style={{marginLeft: "15px", color: "gray"}}>Bliv medlem</p>
+        </div>
+        <div style={{display: "flex", alignItems: "center"}}>
+          <ErrorOutlineIcon />
+          <p style={{marginLeft: "15px", color: "gray"}}>Læs om alle fordele</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-)
-
-}
-export  default LoginPage
+export default LoginPage;
