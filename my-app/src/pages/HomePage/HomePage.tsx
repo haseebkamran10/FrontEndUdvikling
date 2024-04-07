666e898db947289d354eb2071fde198fefdd9eaf
MuhammadFeyaz <113907747+MuhammadFeyaz@users.noreply.github.com>
2024-04-07 23:51:46 +0200

    More slides
    I made more photos for the slide

--- my-app/src/pages/HomePage/HomePage.tsx
+++ my-app/src/pages/HomePage/HomePage.tsx
@@ -6,6 +6,11 @@ import 'slick-carousel/slick/slick-theme.css';
 import './HomePage.css';
 import image1 from '../../kookaburra-placeholder.png'; // Update the path to the correct one for your project
 import image2 from '../../Ben-Stokes-Cover.jpg'; // Update the path to the correct one for your project
+import image3 from '../../Bat-Cover.jpg'
+import image4 from '../../Cover-2024.png'
+import image5 from '../../Player-cover.png'
+
+
 
 const HomePage: React.FC = () => {
   const settings = {
@@ -15,7 +20,7 @@ const HomePage: React.FC = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 2500,
   };
 
   return (
@@ -26,6 +31,15 @@ const HomePage: React.FC = () => {
         </div>
         <div>
           <img src={image2} alt="Slide 2" />
+        </div>
+        <div>
+          <img src={image3} alt="Slide 3" />
+        </div>
+        <div>
+          <img src={image4} alt="Slide 4" />
+        </div>
+        <div>
+          <img src={image5} alt="Slide 5" />
         </div>
       </Slider>
     </div>
