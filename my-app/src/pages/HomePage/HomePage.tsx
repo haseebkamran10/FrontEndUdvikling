0f6d04bf3c4d4a3893a91150458d4c49a7ce9318
MuhammadFeyaz <113907747+MuhammadFeyaz@users.noreply.github.com>
2024-04-08 00:06:55 +0200

    Adding recommendation text
    Adding recommendation text

--- my-app/src/pages/HomePage/HomePage.tsx
+++ my-app/src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,15 @@
 // HomePage.tsx
+
 import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './HomePage.css';
-import image1 from '../../kookaburra-placeholder.png'; // Update the path to the correct one for your project
-import image2 from '../../Ben-Stokes-Cover.jpg'; // Update the path to the correct one for your project
-import image3 from '../../Bat-Cover.jpg'
-import image4 from '../../Cover-2024.png'
-import image5 from '../../Player-cover.png'
-
-
+import image1 from '../../kookaburra-placeholder.png'; // Make sure the path is correct
+import image2 from '../../Ben-Stokes-Cover.jpg'; // Make sure the path is correct
+import image3 from '../../Bat-Cover.jpg'; // Make sure the path is correct
+import image4 from '../../Cover-2024.png'; // Make sure the path is correct
+import image5 from '../../Player-cover.png'; // Make sure the path is correct
 
 const HomePage: React.FC = () => {
   const settings = {
@@ -24,25 +23,30 @@ const HomePage: React.FC = () => {
   };
 
   return (
-    <div className="slideshow-container">
-      <Slider {...settings}>
-        <div>
-          <img src={image1} alt="Slide 1" />
-        </div>
-        <div>
-          <img src={image2} alt="Slide 2" />
-        </div>
-        <div>
-          <img src={image3} alt="Slide 3" />
-        </div>
-        <div>
-          <img src={image4} alt="Slide 4" />
-        </div>
-        <div>
-          <img src={image5} alt="Slide 5" />
-        </div>
-      </Slider>
-    </div>
+    <>
+      <div className="slideshow-container">
+        <Slider {...settings}>
+          <div>
+            <img src={image1} alt="Slide 1" />
+          </div>
+          <div>
+            <img src={image2} alt="Slide 2" />
+          </div>
+          <div>
+            <img src={image3} alt="Slide 3" />
+          </div>
+          <div>
+            <img src={image4} alt="Slide 4" />
+          </div>
+          <div>
+            <img src={image5} alt="Slide 5" />
+          </div>
+        </Slider>
+      </div>
+      <div className="recommendation-section">
+        <h1 className="recommendation-title">ANBEFALING TIL DIG</h1>
+      </div>
+    </>
   );
 };
 
