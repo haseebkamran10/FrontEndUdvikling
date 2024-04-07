ad71acb931f894e184016c7040ee480cf53ab43b
haseebkamran10 <s224270@dtu.dk>
2024-04-07 17:26:09 +0200

    Developing ShoppingCart UI
    Developing ShoppingCart UI

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,17 +1,18 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header/header';
-import CheckoutPage from './pages/CheckoutPage/CheckoutPage';
+import CheckoutPage from './pages/CheckoutPage2/CheckoutPage2';
 import PaymentPage from './pages/PaymentPage/PaymentPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import ProductDetailsPage from './pages/ProductDetailsPage/ProductDetailsPage';
+import CheckoutPage2 from './pages/CheckoutPage2/CheckoutPage2';
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/basket" element={<CheckoutPage />} />
+        <Route path="/basket" element={<CheckoutPage2 />} />
         <Route path="/payment" element={<PaymentPage />} />
         <Route path="/productdetail" element={<ProductDetailsPage />} />
         // ... other routes
