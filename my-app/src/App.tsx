dc0bde3a01f7e8dfd8582cd39a0edc2e73084f99
MuhammadFeyaz <113907747+MuhammadFeyaz@users.noreply.github.com>
2024-04-08 05:30:31 +0200

    Add slides and homepage

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header/header';
 import CheckoutPage from './pages/CheckoutPage/CheckoutPage';
 import PaymentPage from './pages/PaymentPage/PaymentPage';
-import LoginPage from './pages/LoginPage/LoginPage';
+// import LoginPage from './pages/LoginPage/LoginPage';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/basket" element={<CheckoutPage />} />
         <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/" element={<HomePage />} />
+
         // ... other routes
          
       </Routes>
---
+++ my-app/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,38 @@
+// HomePage.tsx
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './HomePage.css';
+import image1 from '../../kookaburra-placeholder.png'; // Update the path to the correct one for your project
+import image2 from '../../Ben-Stokes-Cover.jpg'; // Update the path to the correct one for your project
+import image3 from '../../SS-cover.png';
+import image4 from '../../Bat-Cover.jpg'; 
+import image5 from '../../Cover-2024.png';
+
+const HomePage: React.FC = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  };
+
+  return (
+    <div className="slideshow-container">
+      <Slider {...settings}>
+        <div><img src={image1} alt="Slide 1" /></div>
+          <div><img src={image2} alt="Slide 2" /></div>
+            <div><img src={image3} alt="Slide 3" /></div>
+              <div><img src={image4} alt="Slide 4" /></div>
+                <div><img src={image5} alt="Slide 5" /></div>
+      </Slider>
+    </div>
+  );
+
+};
+
+export default HomePage;
