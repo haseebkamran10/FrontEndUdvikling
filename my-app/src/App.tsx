d2ae66ae39fd2b917388b7692d0ae381d154add5
Fathionsons <gathefathe29@gmail.com>
2024-04-18 21:25:05 +0200

    Update App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -6,21 +6,16 @@ import PaymentPage from './pages/PaymentPage/PaymentPage';
 //import LoginPage from './pages/LoginPage/LoginPage';
 import ProductDetailsPage from './pages/ProductDetailsPage/ProductDetailsPage';
 import ProductsPage from './pages/ProductsPage/ProductsPage';
-import CheckoutPage2 from './pages/CheckoutPage2/CheckoutPage2';
 
 function App() {
   return (
     <Router>
-
-      <Header/>
        <Routes>
+       <Header/>
         <Route path="/basket" element={<CheckoutPage />} />
-      <Header />
-        <Route path="/basket" element={<CheckoutPage2 />} />
         <Route path="/payment" element={<PaymentPage />} />
         <Route path="/productdetail" element={<ProductDetailsPage />} />
         <Route path="/productspage" element={<ProductsPage />} />
-    
         // ... other routes
          
       </Routes>
