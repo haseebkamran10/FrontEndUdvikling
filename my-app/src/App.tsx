eb7757d4e45d1c6717e7c1cfd87c4239d549bb98
MuhammadFeyaz <113907747+MuhammadFeyaz@users.noreply.github.com>
2024-04-22 13:03:17 +0200

    Footer
    Footer

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,24 @@
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header/header';
 import CheckoutPage from './pages/CheckoutPage/CheckoutPage';
 import PaymentPage from './pages/PaymentPage/PaymentPage';
+import Footer from './components/Footer/Footer';
 // import LoginPage from './pages/LoginPage/LoginPage';
 import HomePage from './pages/HomePage/HomePage';
-import ProductsPage from './pages/ProductsPage/ProductsPage';
 
 function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/basket" element={<CheckoutPage />} />
-        <Route path="/payment" element={<PaymentPage />} />
-        <Route path="/" element={<HomePage />} />
-
-
-        // ... other routes
-         
-      </Routes>
+      <div className="main-content">
+        <Routes>
+          <Route path="/basket" element={<CheckoutPage />} />
+          <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/" element={<HomePage />} />
+          {/* ... other routes */}
+        </Routes>
+      </div>
+      <Footer />
     </Router>
   );
 }
---
+++ my-app/src/components/Footer/Footer.tsx
@@ -0,0 +1,44 @@
+import './Footer.css'; // Make sure the CSS file is correctly linked
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-column">
+        <h3>Nordic Cricket</h3>
+        <p>Nordic Cricket tilbyder et omfattende udvalg af cricketudstyr og -tilbehør, der imødekommer spillere på alle niveauer fra begyndere til avancerede. 
+          Vi forstår vigtigheden af kvalitet og holdbarhed i cricketudstyr og er dedikeret til at levere produkter, der både forbedrer spiloplevelsen og sikrer spillerens sikkerhed.</p>
+      </div>
+      <div className="footer-column">
+        <h3>Kundeservice</h3>
+        <p>Se åbningstider </p>
+        <p>Mail:  kundeservice@nordiccricket.nu</p>
+        <p>Telefon:  60 50 40 68</p>
+        <p>Køb gavekort </p>
+        <p>Job</p>
+      </div>
+      <div className="footer-column">
+        <h3>Hjælp</h3>
+        <p>Levering og Tracking</p>
+          <p>Reklamation</p>
+          <p>Retunering</p> 
+          <p>Gavekort</p>
+          <p>Ørige spørgsmål</p>
+          <p>Fortrydelseret</p>
+          <p>Handelbetigelser</p>
+          <p>Om vores priser</p>
+          <p>Click & Collect </p>
+      </div>
+      <div className="footer-column">
+        <h3>Mere fra Nordic Cricket</h3>
+        <p>Bliv medlem </p>
+        <p>Bonus</p>
+        <p>Kvitteringer</p>
+        <p>Afmeld nyhedsbrev / SMS  </p>
+        <p>Hjælp til log ind </p>
+        <p>Om Nordic Cricket </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
